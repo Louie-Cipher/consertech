60769db56bd825020d9cbb6bd8d1fd7eef2a5ed9
Louie-Cipher <louie.spena@outlook.com>
2022-11-22 14:22:50 -0300

    change icon colors

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,22 +6,28 @@ export default () => {
         <FooterLink
             name='Facebook'
             link='https://www.facebook.com/consertechoficial/'
-            img='https://img.icons8.com/color/48/000000/facebook-new.png'
+            img='https://img.icons8.com/FFFFFF/facebook-new'
         />
         <FooterLink
             name='Instagram'
             link='https://www.instagram.com/consertech.oficial/'
-            img='https://img.icons8.com/color/48/000000/instagram-new.png'
+            img='https://img.icons8.com/FFFFFF/instagram-new'
         />
         <FooterLink
             name='WhatsApp'
             link='https://api.whatsapp.com/send?phone=551140028922'
-            img='https://img.icons8.com/color/48/000000/whatsapp.png'
+            img='https://img.icons8.com/FFFFFF/whatsapp'
         />
         <FooterLink
             name='Telegram'
             link='https://t.me/consertech'
-            img='https://img.icons8.com/color/48/000000/telegram-app.png'
+            img='https://img.icons8.com/FFFFFF/telegram-app'
+        />
+
+        <FooterLink
+            name='Código fonte'
+            link='https://github.com/louie-cipher/consertech'
+            img='https://img.icons8.com/FFFFFF/github'
         />
     </footer>;
 };
