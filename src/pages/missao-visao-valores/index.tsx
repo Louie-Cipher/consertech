51f59bc97573aeb893b80bf2ae9e3d0bc3a14b82
Louie-Cipher <louie.spena@outlook.com>
2022-11-22 16:33:44 -0300

    podcast audio

--- src/pages/missao-visao-valores/index.tsx
+++ src/pages/missao-visao-valores/index.tsx
@@ -1,4 +1,21 @@
+import { Howl } from 'howler';
+import { useState } from 'react';
+import podcast from '../../assets/podcast.mp3';
+import './styles.css';
+
 export default () => {
+    const [playing, setPlaying] = useState(false);
+
+    const handleButton = () => {
+        setPlaying(!playing);
+
+        if (playing) {
+            soundPlayer.pause();
+        } else {
+            soundPlayer.play();
+        }
+    };
+
     return (
         <div className='content'>
             <div className='mvv-item'>
@@ -21,6 +38,18 @@ export default () => {
                     relatórios. Respeito ao cliente.
                 </p>
             </div>
+
+            <div className='podcast-container'>
+                <p>Ouça aqui nosso podcast sobre nossa missão, visão e valores</p>
+                <button className='play-button' onClick={handleButton}>
+                    {playing ? 'Pausar' : 'Reproduzir'}
+                </button>
+            </div>
         </div>
     );
 };
+
+const soundPlayer = new Howl({
+    src: [podcast],
+    volume: 2,
+});
